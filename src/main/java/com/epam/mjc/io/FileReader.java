ba2ac7cedb4ab7a6dbd3f6d4e5de03116ab8669b
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-14 15:42:32 +0300

    Add try-catch

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,19 +6,22 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
-public class FileReader {
+public class FileReader
+{
     public static void main(String[] args)
     {
-        File file = new File("G:\\My Drive\\IT\\Epam\\MJC School\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
-        System.out.println(new FileReader().getDataFromFile(file));
+        System.out.println(new FileReader().getDataFromFile(new File("G:\\My Drive\\IT\\Epam\\MJC School\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt")));
     }
 
-    public Profile getDataFromFile(File file) {
+    public Profile getDataFromFile(File file)
+    {
         StringBuilder str = new StringBuilder();
-        try (FileInputStream fileInputStream = new FileInputStream(file)){
+        try (FileInputStream fileInputStream = new FileInputStream(file))
+        {
             int ch;
-            while ((ch = fileInputStream.read()) != -1){
-                str.append((char)ch);
+            while ((ch = fileInputStream.read()) != -1)
+            {
+                str.append((char) ch);
             }
         }
         catch (FileNotFoundException e)
@@ -29,12 +32,19 @@ public class FileReader {
         {
             throw new RuntimeException(e);
         }
+
+
         String input = str.toString();
         String[] keyValuePairs = input.split("\r\n");
 
         for (int i = 0; i < keyValuePairs.length; i++)
         {
-            keyValuePairs[i] = keyValuePairs[i].split(": ")[1];
+            try {
+                keyValuePairs[i] = keyValuePairs[i].split(": ")[1];
+            } catch (Exception e){
+
+            }
+
         }
         String name = keyValuePairs[0];
         int age = Integer.parseInt(keyValuePairs[1]);
